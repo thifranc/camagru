b626b5ad58ce6f84ad6f4ef7563afec18fa07631
Thibault FRANCOIS <thifranc@e2r10p6.42.fr>
2016-07-26 18:35:07 +0200

    dded cam minimum to begin gallery debug

--- public/cam/index.php
+++ public/cam/index.php
@@ -1,6 +1,39 @@
 <?PHP
+
 session_start();
+
+function connect()
+{
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=thug_cam', 'root', 'root');
+		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		return ($bdd);
+	} catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+		die();
+	}
+}
+
 if (isset($_SESSION['log_id']))
-	echo ('take a pic');
+{
+	if (isset($_FILES['nik']['name']) && !empty($_FILES['nik']['name']))
+	{
+		if (!file_exists("../../private/img/"))
+			mkdir("../../private/img");
+		try {
+			$bdd = connect();
+			$path = "../../private/img/" . uniqid() . ".png";
+			$query = $bdd->prepare('INSERT INTO img (user_id, link) VALUES( :user_id, :link )');
+			$query->bindParam(':user_id', $_SESSION['log_id']);
+			$query->bindParam(':link', $path);
+			$query->execute();
+			move_uploaded_file($_FILES['nik']['tmp_name'], $path);
+		} catch (PDOException $e) {
+			echo 'Connection failed: ' . $e->getMessage();
+			return FALSE;
+		}
+	}
+	require_once("view.php");
+}
 else
-	echo ('ERROR, not logged');
+	header ("Location: ../home/controller.php");
---
+++ public/cam/view.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="view.css">
+		<title>Camagru</title>
+	</head>
+	<body>
+		<header style="text-align:center;">Img</header>
+
+		<div class="wrapper">
+			<article>
+		<form method="post" action="index.php" enctype="multipart/form-data">
+			<input type="file" name="nik">Choose a file to upload</input>
+			<input type="submit" value="submit" name="submit">
+		</form>
+			</article>
+			<aside>aside</aside>
+		</div>
+
+		<footer>footer</footer>
+	</body>
+</html>
