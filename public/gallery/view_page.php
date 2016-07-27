fc24d30709bbabbf9e9be652c382f6364bdfa235
Thibault FRANCOIS <thifranc@e3r1p14.42.fr>
2016-07-27 08:49:26 +0200

    link improved

--- public/gallery/view_page.php
+++ public/gallery/view_page.php
@@ -21,19 +21,17 @@
 </html>
 
 
-
 <?PHP
 
 //pour remove page : onclick : if SESSION(log_id) == imgclicked['user_id'] => then js remove 
 //les img seront ds des div, avec chacune une id correspondante a img_id (donc unique) donc removable facilement en js
-
 
 	function display_page_img($img_data)
 	{
 		//attention il faut pas de doublons, la on est parti pour en avoir cf fetchAll return
 		foreach ($img_data as $img)
 		{
-			echo '<img src='.$img['link'].' href=controller.php?display=img&img_id='.$imd['img_id'].'>';
+			echo '<a href="controller.php?display=img&img_id='.$img['img_id'].'"><img src='.$img['link'].'></a>';
 		}
 	}
 
@@ -43,7 +41,7 @@
 		$i = 0;
 		while ($i < $nb_page)
 		{
-			echo '<a href="controller.php?display=page&p="'.$i.'> '.$i. ' </a>';
+			echo '<a href="controller.php?display=page&p='.$i.'"> '.$i. ' </a>';
 			$i++;
 		}
 	}
