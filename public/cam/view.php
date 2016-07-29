299d5f07372297e35851992772aa2c3a114facac
thifranc <thifranc@student.42.fr>
2016-07-29 13:12:38 +0200

    photo can be updated via apache

--- public/cam/view.php
+++ public/cam/view.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" type="text/css" href="view.css">
 		<title>Camagru</title>
 	</head>
