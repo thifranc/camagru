c6724905325ffc5cd5cc53f536f0be943283667f
thifranc <thifranc@student.42.fr>
2016-08-02 22:50:23 +0200

    img webcam + merge handled. resize to do

---
+++ public/cam/add.php
@@ -0,0 +1,14 @@
+<?PHP
+
+move_uploaded_file($_FILES['pictures']['tmp_name'], "../../private/img/camera.png");
+$main_photo = imagecreatefrompng("../../private/img/camera.png");
+$sub_photo = imagecreatefrompng("glasses.png");
+
+$ret = imagecopymerge($main_photo, $sub_photo, 0, 0, 500, 500, 1000, 1000, 100);
+if ($ret == false)
+	echo 'failed';
+else
+	echo 'success';
+imagepng($main_photo, "../../private/img/camera.png");
+imagedestroy($main_photo);
+imagedestroy($sub_photo);
--- public/cam/webcam.js
+++ public/cam/webcam.js
@@ -1,65 +1,78 @@
 (function() {
 
-  var streaming = false,
-      video        = document.getElementById('video'),
-      canvas       = document.getElementById('canvas'),
-      photo    	   = document.getElementById('photo'),
-      startbutton  = document.getElementById('startbutton'),
-      width = 320,
-      height = 0;
+	var streaming = false,
+video        = document.querySelector('#video'),
+cover        = document.querySelector('#cover'),
+canvas       = document.querySelector('#canvas'),
+photo        = document.querySelector('#photo'),
+startbutton  = document.querySelector('#startbutton'),
+width = 320,
+height = 0;
 
-  navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+navigator.getMedia = ( navigator.getUserMedia ||
+	navigator.webkitGetUserMedia ||
+	navigator.mozGetUserMedia ||
+	navigator.msGetUserMedia);
 
-if (navigator.getMedia)
-{
- 	 navigator.getMedia(
-    {video: true,audio: false},
-
-    function(stream) {
-	if (navigator.mozGetUserMedia)
-        video.mozSrcObject = stream;
-	else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-      }
-      video.play();
-    },
-
-    function(err) {
-      console.log("An error occured! " + err);
+navigator.getMedia(
+	{
+		video: true,
+	audio: false
+	},
+	function(stream) {
+		if (navigator.mozGetUserMedia) {
+			video.mozSrcObject = stream;
+		} else {
+			var vendorURL = window.URL || window.webkitURL;
+			video.src = vendorURL.createObjectURL(stream);
+		}
+		video.play();
+	},
+	function(err) {
+		console.log("An error occured! " + err);
 	}
 	);
+
+video.addEventListener('canplay', function(ev){
+	if (!streaming) {
+		height = video.videoHeight / (video.videoWidth/width);
+		video.setAttribute('width', width);
+		video.setAttribute('height', height);
+		canvas.setAttribute('width', width);
+		canvas.setAttribute('height', height);
+		streaming = true;
+	}
+}, false);
+
+function takepicture() {
+	canvas.width = width;
+	canvas.height = height;
+	canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+	var data = canvas.toDataURL('image/png');
+	photo.setAttribute('src', data);
+
+
+	canvas.toBlob(function(blob) {
+		var formData = new FormData();
+		console.log('bonjour');
+		formData.append('pictures', blob);
+		var sender = new XMLHttpRequest();
+		sender.open("POST", "add.php", true);
+		sender.send(formData);
+		sender.onload = function () {
+			if (sender.status >= 200 && sender.status <= 400)
+	{
+		var ret = sender.responseText;
+		console.log(ret);
+		console.log('lol');
+	}
+		}
+	});
 }
 
-  video.addEventListener('canplay', function(ev){
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
-      video.setAttribute('width', width);
-      video.setAttribute('height', height);
-      canvas.setAttribute('width', width);
-      canvas.setAttribute('height', height);
-      streaming = true;
-    }
-  }, false);
-
-
-//  var canvas = document.getElementById('canvas');
-//  var rect = canvas.getBoundingClientRect();
-//  console.log(rect.top, rect.bottom, rect.right, rect.left);
-//  var glasses = document.getElementById('glasses');
-
-
-  function takepicture() {
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
-	var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
-
-  startbutton.addEventListener('click', function(ev){
-      takepicture();
-    ev.preventDefault();
-  }, false);
+startbutton.addEventListener('click', function(ev){
+	takepicture();
+	ev.preventDefault();
+}, false);
 
 })();
