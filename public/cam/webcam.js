3864eee307b847c1d402d2010e9ab769ab0c8504
Thibault FRANCOIS <thifranc@e1r9p12.42.fr>
2016-08-08 15:16:44 +0200

    mozilla friendly

--- public/cam/webcam.js
+++ public/cam/webcam.js
@@ -12,24 +12,35 @@ height = 0;
 
 navigator.getMedia = ( navigator.getUserMedia ||
 	navigator.webkitGetUserMedia ||
-	navigator.mozGetUserMedia ||
+	navigator.mediaDevices.getUserMedia ||
 	navigator.msGetUserMedia);
 
-navigator.getMedia(
-	{video: true,audio: false},
-	function(stream) {
-		if (navigator.mozGetUserMedia) {
-			video.mozSrcObject = stream;
-		} else {
-			var vendorURL = window.URL || window.webkitURL;
-			video.src = vendorURL.createObjectURL(stream);
+if (navigator.mediaDevices.getUserMedia)
+{
+	var constraints = window.constraints = {audio: false,video: true};
+
+		navigator.mediaDevices.getUserMedia(constraints)
+			.then(function(stream) {
+			  var videoTracks = stream.getVideoTracks();
+			  window.stream = stream; // make variable available to browser console
+			  video.srcObject = stream;
+			})
+	video.play();
+}
+else
+{
+	navigator.getMedia(
+		{video: true,audio: false},
+		function(stream) {
+				var vendorURL = window.URL || window.webkitURL;
+				video.src = vendorURL.createObjectURL(stream);
+				video.play();
+		},
+		function(err) {
+			console.log("An error occured! " + err);
 		}
-		video.play();
-	},
-	function(err) {
-		console.log("An error occured! " + err);
-	}
-	);
+		);
+}
 
 video.addEventListener('canplay', function(ev){
 	if (!streaming) {
